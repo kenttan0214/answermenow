0748368025ab5567aebb94f171cf392b5d9e4df2
SEEKASIA\kenttan <kenttan@seekasia.com>
2017-06-30 14:17:48 +0800

    add select user by field function

--- api/services/UserService.js
+++ api/services/UserService.js
@@ -16,5 +16,27 @@ module.exports = {
         return done(false, err);
       }
     });
+  },
+  getUserFields: function (criteria, fields, done) {
+    User.findOne(criteria).exec(function (err, record) { // eslint-disable-line no-undef
+      if (!err && record) {
+        var filteredRecord = {};
+        for (var i = 0; i < fields.length; ++i) {
+          var field = fields[i];
+          filteredRecord[field] = record[field];
+        }
+        return done(true, filteredRecord);
+      } else {
+        return done(false, err);
+      }
+    });
   }
 };
+
+// User.find(criteria, {fields: {_id: 1}}).exec(function (err, record) { // eslint-disable-line no-undef
+//   if (!err && record) {
+//     return done(true, record);
+//   } else {
+//     return done(false, err);
+//   }
+// });
